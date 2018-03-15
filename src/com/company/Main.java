d6fd890e9d69fee84b280ba64a9c9b95a1fa345d
IPod <podsevalov.ivan@yandex.ru>
2018-03-15 23:59:56 +0300

    Second commit:)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,9 @@ import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.events.XMLEvent;
 import java.io.*;
-import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-
 
 public class Main {
 
@@ -17,74 +15,77 @@ public class Main {
     private Map nodes = new HashMap<Long, Node_Coordinates>();
     private Map<Long, ArrayList<Long>> adjacency_list = new HashMap<>(); // <node_id, adjacency_node_ids>
     private HashSet roads = new HashSet<>(Arrays.asList("motorway", "motorway_link", "trunk", "trunk_link", "secondary", "secondary_link",
-            "primary", "primary_link", "tertiary", "tertiary_link", "unclassified", "road"));
+            "primary", "primary_link", "tertiary", "tertiary_link", "unclassified", "residential", "service", "living_street", "road"));
+
+    public static void main(String[] args) throws Exception {
+        long start_time = System.currentTimeMillis();
+        new Main().run();
+        long finish_time = System.currentTimeMillis();
+        System.out.printf("\n" + (finish_time - start_time) + " ms");
+    }
 
     private void run() throws Exception {
-        //Scanner input = new Scanner(new File("input.txt"));
         try(StreamProcessor processor = new StreamProcessor(Files.newInputStream(Paths.get("Krasnoyarsk.osm")))) {
-            XMLStreamReader reader = processor.getReader();
-            while (processor.startElement("node", "way")) { // Maped all nodes
-                long mid = Long.parseLong(processor.getAttribute("id"));
-                //System.out.printf("id = " + processor.getAttribute("id") + "; lat= " + processor.getAttribute("lat") + "; lon= " + processor.getAttribute("lon") + "\n");
+            while (processor.startElement("node", "way"))  // Maped all nodes to all_nodes
                 all_nodes.put(Long.parseLong(processor.getAttribute("id")), new Node_Coordinates(Double.parseDouble(processor.getAttribute("lat")), Double.parseDouble(processor.getAttribute("lon"))));
-            }
-           while (processor.startElement("way", "relation")) { // Maped all ways
-               ArrayList temp_list = new ArrayList<Long>();
-               while (processor.startElement("nd", "tag"))
-                   temp_list.add(Long.parseLong(processor.getAttribute("ref")));
-               while (processor.startElement("tag", "way"))
-                   if ("highway".equals(processor.getAttribute("k")) && roads.contains(processor.getAttribute("v"))){
-                       for(int i = 0; i < temp_list.size(); ++i){
-                           if(adjacency_list.containsKey((Long) temp_list.get(i))){
-                               if(i - 1 < 0)
-                                   adjacency_list.get(temp_list.get(i)).add((Long) temp_list.get(i+1));
-                               else if(i + 1 == temp_list.size())
-                                   adjacency_list.get(temp_list.get(i)).add((Long) temp_list.get(i-1));
-                               else
-                                   adjacency_list.get(temp_list.get(i)).addAll(Arrays.asList((Long) temp_list.get(i-1), (Long) temp_list.get(i+1)));
-                           }else{
-                               if(i - 1 < 0)
-                                   adjacency_list.put((Long) temp_list.get(i), new ArrayList<>(Arrays.asList((Long) temp_list.get(i+1))));
-                               else if(i + 1 == temp_list.size())
-                                   adjacency_list.put((Long) temp_list.get(i), new ArrayList<>(Arrays.asList((Long) temp_list.get(i-1))));
-                               else
-                                   adjacency_list.put((Long) temp_list.get(i), new ArrayList<>(Arrays.asList((Long) temp_list.get(i-1),(Long) temp_list.get(i+1))));
-                           }
-                           nodes.put(temp_list.get(i), all_nodes.get(temp_list.get(i)));
+            while (processor.startElement("way", "relation")) { // Maped all ways
+                ArrayList temp_list = new ArrayList<Long>();
+                while (processor.startElement("nd", "tag"))
+                    if(all_nodes.containsKey(Long.parseLong(processor.getAttribute("ref"))))
+                        temp_list.add(Long.parseLong(processor.getAttribute("ref")));
+
+                while (processor.startElement("tag", "way"))
+                    if ("highway".equals(processor.getAttribute("k")) && roads.contains(processor.getAttribute("v")) && temp_list.size() > 1){
+                        for(int i = 0; i < temp_list.size(); ++i){
+                            if (adjacency_list.containsKey((Long) temp_list.get(i))) {
+                                if (i - 1 < 0)
+                                    adjacency_list.get(temp_list.get(i)).add((Long) temp_list.get(i + 1));
+                                else if (i + 1 == temp_list.size())
+                                    adjacency_list.get(temp_list.get(i)).add((Long) temp_list.get(i - 1));
+                                else
+                                    adjacency_list.get(temp_list.get(i)).addAll(Arrays.asList((Long) temp_list.get(i - 1), (Long) temp_list.get(i + 1)));
+                                } else {
+                                    if (i - 1 < 0)
+                                        adjacency_list.put((Long) temp_list.get(i), new ArrayList<>(Arrays.asList((Long) temp_list.get(i + 1))));
+                                    else if (i + 1 == temp_list.size())
+                                        adjacency_list.put((Long) temp_list.get(i), new ArrayList<>(Arrays.asList((Long) temp_list.get(i - 1))));
+                                    else
+                                        adjacency_list.put((Long) temp_list.get(i), new ArrayList<>(Arrays.asList((Long) temp_list.get(i - 1), (Long) temp_list.get(i + 1))));
+                                }
+                                nodes.put(temp_list.get(i), all_nodes.get(temp_list.get(i)));
                        }
                    }
            }
-            System.out.printf(all_nodes.size() + "    " + nodes.size());
+           System.out.printf(all_nodes.size() + "    " + nodes.size());
+            for (Object key: nodes.keySet())
+                ((Node_Coordinates) nodes.get(key)).set_coordinates();
+
+
             printCSV();
             createSVG();
-
         }
     }
 
     private void printCSV() throws Exception{
-        String CSV_Nodes_File = "C:/Program Files/osmosis-latest/bin/GraphOfCity/Nodes.csv";
-        String CSV_AdjList_File = "C:/Program Files/osmosis-latest/bin/GraphOfCity/AdjList.csv";
+        String CSV_Nodes_File = "Nodes.csv";
+        String CSV_AdjList_File = "AdjList.csv";
 
         FileWriter writer = new FileWriter(CSV_Nodes_File);
-        CSVUtils.writeLine(writer, Arrays.asList("Node_id", "'lat' and 'lon'"), ';', ' ');
+        CSVUtils.writeLine(writer, Arrays.asList("Node_id", "'lat' and 'lon'"), ';');
         for(Object key : nodes.keySet())
-            try {
-                CSVUtils.writeLine(writer, Arrays.asList(key.toString(), nodes.get(key).toString()), ';', ' ');
-            }catch (Exception e){
-            }
-
+            CSVUtils.writeLine(writer, Arrays.asList(key.toString(), nodes.get(key).toString()), '>');
 
         writer = new FileWriter(CSV_AdjList_File);
-        CSVUtils.writeLine(writer, Arrays.asList("Node_id", "adjacency_list"), ';', ' ');
-        for(Object key : adjacency_list.keySet()){
-            CSVUtils.writeLine(writer, Arrays.asList(key.toString(), adjacency_list.get(key).toString()), ';', ' ');
-        }
+        CSVUtils.writeLine(writer, Arrays.asList("Node_id", "adjacency_list"), ';');
+        for(Object key : adjacency_list.keySet())
+            CSVUtils.writeLine(writer, Arrays.asList(key.toString(), adjacency_list.get(key).toString()), '>');
+
         writer.flush();
         writer.close();
     }
 
     private void createSVG() throws Exception{
-        String SVG_File = "C:/Program Files/osmosis-latest/bin/GraphOfCity/visualization.svg";
+        String SVG_File = "visualization.svg";
         PrintWriter out = new PrintWriter(new FileWriter(SVG_File));
         out.println("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
                 "<svg version = \"1.1\"\n" +
@@ -95,30 +96,16 @@ public class Main {
                 "     height = \"100%\"  width = \"100%\">\n" +
                 "     <g fill-opacity=\"0.6\" stroke=\"black\" stroke-width=\"0.5px\">");
 
-        for(Object key : nodes.keySet())
-            try {
-                out.println("       <circle cx=\"-1035700\" cy=\"-755900\" r=\"2px\" fill=\"blue\"   transform=\"translate(" + ((Node_Coordinates) nodes.get(key)).get_variable("euclid_X") + "," + ((Node_Coordinates) nodes.get(key)).get_variable("euclid_Y") + ")\" />");
-            }catch (Exception e){
-            }
+        for(Object key : nodes.keySet()) // cx="-1035700" cy="-755900"
+            out.println("       <circle r=\"1px\" fill=\"blue\"   transform=\"translate(" + ((Node_Coordinates) nodes.get(key)).get_variable("euclid_X")  + "," + ((Node_Coordinates) nodes.get(key)).get_variable("euclid_Y") + ")\" />");
 
         for(Object key : adjacency_list.keySet())
             for(Object value : adjacency_list.get(key))
-                try {
-                    out.println("       <line x1=\"" + (-1035700 + ((Node_Coordinates) nodes.get(key)).get_variable("euclid_X")) + "\" y1=\"" + (-755900 + ((Node_Coordinates) nodes.get(key)).get_variable("euclid_Y")) + "\" x2=\"" + (-1035700 + ((Node_Coordinates) nodes.get(value)).get_variable("euclid_X")) +"\" y2=\"" + (-755900 + ((Node_Coordinates) nodes.get(value)).get_variable("euclid_Y")) + "\" style=\"stroke:rgb(255,0,0);stroke-width:1\" />");
-                }catch (Exception e){
-                }
+                out.println("       <line x1=\"" + ((Node_Coordinates) nodes.get(key)).get_variable("euclid_X") + "\" y1=\"" + ((Node_Coordinates) nodes.get(key)).get_variable("euclid_Y") + "\" x2=\"" + ((Node_Coordinates) nodes.get(value)).get_variable("euclid_X") + "\" y2=\"" + ((Node_Coordinates) nodes.get(value)).get_variable("euclid_Y") + "\" style=\"stroke:rgb(0,64,255);stroke-width:0.0009\" />");
 
         out.print("     </g>\n" +
                 "</svg>");
-
         out.close();
-    }
-
-    public static void main(String[] args) throws Exception {
-        long start_time = System.currentTimeMillis();
-        new Main().run();
-        long finish_time = System.currentTimeMillis();
-        System.out.printf("\n" + (finish_time - start_time) + " ms");
     }
 }
 
@@ -134,8 +121,7 @@ class CSVUtils {
         return result;
 
     }
-
-    static void writeLine(Writer w, List<String> values, char separators, char customQuote) throws IOException {
+    static void writeLine(Writer w, List<String> values, char separators) throws IOException {
 
         boolean first = true;
 
@@ -145,12 +131,8 @@ class CSVUtils {
         StringBuilder sb = new StringBuilder();
         for (String value : values) {
             if (!first)
-                sb.append(separators);
-            if (customQuote == ' ')
-                sb.append(followCVSformat(value));
-            else
-                sb.append(customQuote).append(followCVSformat(value)).append(customQuote);
-
+                sb.append(" ").append(separators).append(" ");
+            sb.append(followCVSformat(value));
             first = false;
         }
         sb.append("\n");
@@ -161,22 +143,48 @@ class CSVUtils {
 
 class Node_Coordinates{
     double lat, lon, euclid_X, euclid_Y;
+    final private static double R_MAJOR = 6378137.0;
+    final private static double R_MINOR = 6356752.3142;
+    static double X = Double.MAX_VALUE, Y = Double.MAX_VALUE;
+
+    private double  mercX(double lon) {
+        return R_MAJOR * Math.toRadians(lon);
+    }
+
+    private double mercY(double lat) {
+        if (lat > 89.5) {
+            lat = 89.5;
+        }
+        if (lat < -89.5) {
+            lat = -89.5;
+        }
+        double temp = R_MINOR / R_MAJOR;
+        double es = 1.0 - (temp * temp);
+        double eccent = Math.sqrt(es);
+        double phi = Math.toRadians(lat);
+        double sinphi = Math.sin(phi);
+        double con = eccent * sinphi;
+        double com = 0.5 * eccent;
+        con = Math.pow(((1.0-con)/(1.0+con)), com);
+        double ts = Math.tan(0.5 * ((Math.PI*0.5) - phi))/con;
+        double y = 0 - R_MAJOR * Math.log(ts);
+        return y;
+    }
 
     Node_Coordinates(double input_lat, double input_lon){
         lat = input_lat;
         lon = input_lon;
-        euclid_coordinates(lat, lon);
+        euclid_X = mercX(input_lon);
+        euclid_Y = mercY(input_lat);
+        if(X > euclid_X && Y > euclid_Y) {
+            X = euclid_X;
+            Y = euclid_Y;
+        }
     }
 
-    void euclid_coordinates(double input_lat, double input_lon){
-        final double Multiplier = 100, Radius = 6378;
-        double latRad, lonRad;
-
-        latRad = input_lat * Math.PI / 180;
-        lonRad = input_lon * Math.PI / 180;
-
-        euclid_X = Multiplier * Radius * lonRad;
-        euclid_Y = Multiplier * Radius * Math.log(Math.tan(Math.PI/4+latRad/2));
+    void set_coordinates(){
+        euclid_X = (euclid_X - X) * 1000000000000000000L;
+        euclid_Y = (euclid_Y - Y) * 1000000000000000000L;
     }
 
     double get_variable(String variable_name){
@@ -208,10 +216,6 @@ class StreamProcessor implements AutoCloseable {
         reader = FACTORY.createXMLStreamReader(is);
     }
 
-    XMLStreamReader getReader() {
-        return reader;
-    }
-
     boolean startElement(String element, String stop_tag) throws XMLStreamException {
         while (reader.hasNext()) {
             int event = reader.next();
@@ -223,11 +227,8 @@ class StreamProcessor implements AutoCloseable {
         return false;
     }
 
-    public String getAttribute(String name) throws XMLStreamException {
+    String getAttribute(String name) throws XMLStreamException {
         return reader.getAttributeValue(null, name);
-    }
-    public String getText() throws XMLStreamException {
-        return reader.getElementText();
     }
 
     @Override
@@ -235,8 +236,6 @@ class StreamProcessor implements AutoCloseable {
         if (reader != null)
             try {
                 reader.close();
-            }catch (XMLStreamException e) {
-                System.out.printf("Here is err");
-            }
+            }catch (XMLStreamException e) {}
     }
 }
